9863c3034eabc2d33045ecd4e43a0474ded3a9bc
Ajit Kumar Panigrahi <ajitkumarpanigrahi122@gmail.com>
2026-06-27 23:42:17 +0530

    fix sidebar missing import

--- profrospot/src/App.jsx
+++ profrospot/src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import Sidebar from "./components/Sidebar.jsx";
 import Display from "./components/display";
+
 export default function App() {
   return (
-   <div className="w-screen h-screen">
-  <Display />
-</div>
+    <div className="w-screen h-screen flex">
+      <Sidebar />
+      <Display />
+    </div>
   );
 }
