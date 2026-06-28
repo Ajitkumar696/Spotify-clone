e39424cd2ac84f31f77343535622b4b520c1fd21
Ajit Kumar Panigrahi <ajitkumarpanigrahi122@gmail.com>
2026-06-28 10:45:04 +0530

    Fix Navbar.jsx filename casing

--- profrospot/src/App.jsx
+++ profrospot/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Sidebar from "./components/Sidebar";
+
 import Display from "./components/display";
 
 export default function App() {
   return (
     <div className="w-screen h-screen flex">
-      <Sidebar />
+   
       <Display />
     </div>
   );
